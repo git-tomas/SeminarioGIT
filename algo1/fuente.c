a98a7614356ba51bdeec91ff30656191e5ee2b20
profesor <profesor@aulaszam.usal.es>
2022-12-20 17:51:30 +0100

    Prueba de fichero nuevo y cambio en fichero existente.

---
+++ algo1/fuente.c
@@ -0,0 +1,25 @@
+ñaklshdf 
+sadg sdf
+g sdffg 
+sdfgh
+ sdg
+ hf defaultfgh 
+ dfgh
+  dfghgh 
+  dfgh dfghh defaultfgh 
+  
+  
+  s dfgh 
+  sdfgh sdffg 
+  dsa
+  fgetc
+   
+   sdf gsd
+   f gcvtsd
+    fgetc
+	 sdffg 
+	 sdff 
+	 
+	 
+	  sdf gsgd
+	  
